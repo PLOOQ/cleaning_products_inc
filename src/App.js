41a7dd51948e7c14342fe9886aea8b34fdcfd26d
Pablo Loo <PabloLoo1@Pablos-MacBook-Pro.local>
2021-08-24 12:58:08 -0500

    update before bootstrap

--- src/App.js
+++ src/App.js
@@ -4,44 +4,46 @@ import gen_1 from "./components/images/generic_1.jpg";
 function App() {
   return (
     <div className="App">
-      <div className="encabezado_section">
-        <h1>Cleaning Products Inc.</h1>
-        <h2>
-          Contáctanos por nuestras redes sociales directamente desde aquí!
-        </h2>
-        <img
-          src={cleaning_products_banner}
-          alt="productos_genericos_img "
-          className="productos_genericos_banner"
-        ></img>
-      </div>
-
-      <div className="about_us_section"></div>
-
-      <div className="productos_servicios_section">
-        <div className="productos_servicios_container">
-          <h1>Productos y Servicios</h1>
+      <body>
+        <header className="encabezado_section">
+          <h1>Cleaning Products Inc.</h1>
+          <h2>
+            Contáctanos por nuestras redes sociales directamente desde aquí!
+          </h2>
           <img
-            src={gen_1}
-            alt="slider"
-            className="productos_genericos_slider"
+            src={cleaning_products_banner}
+            alt="productos_genericos_img "
+            className="productos_genericos_banner"
           ></img>
-          <p className="productos_descripcion">
-            Contamos con una variedad de productos <br /> y servicios ... Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit
-            nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus
-            pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla
-            aliquet porttitor venenatis. Donec a dui et dui fringilla
-            consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus
-            dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis
-            sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis
-            semper risus. In adipiscing ultrices tellus, in suscipit massa
-            vehicula eu.
-          </p>
-        </div>
-      </div>
+        </header>
+        <main>
+          <div className="about_us_section"></div>
 
-      <footer className="pie_de_pagina_section"></footer>
+          <div className="productos_servicios_section">
+            <div className="productos_servicios_container">
+              <h1>Productos y Servicios</h1>
+              <img
+                src={gen_1}
+                alt="slider"
+                className="productos_genericos_slider"
+              ></img>
+              <p className="productos_descripcion">
+                Contamos con una variedad de productos <br /> y servicios ...
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus
+                rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae
+                volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui
+                fringilla consectetur id nec massa. Aliquam erat volutpat. Sed
+                ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed
+                lacinia lectus. Duis sit amet sodales felis. Duis nunc eros,
+                mattis at dui ac, convallis semper risus. In adipiscing ultrices
+                tellus, in suscipit massa vehicula eu.
+              </p>
+            </div>
+          </div>
+        </main>
+        <footer className="pie_de_pagina_section"></footer>
+      </body>
     </div>
   );
 }
