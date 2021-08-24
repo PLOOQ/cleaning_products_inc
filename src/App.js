bdfd0c6f4875867d438ce3e90e572f44433e30c5
Pablo Loo <PabloLoo1@Pablos-MacBook-Pro.local>
2021-08-24 11:28:07 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div className="App-body"></div>
+      <footer className="App-footer"></footer>
+    </div>
+  );
+}
+
+export default App;
